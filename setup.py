7f6dd24ccadbb030241fc9c4cdfeda4c609a2b6e
David Seifert <SoapZA@users.noreply.github.com>
2017-03-29 00:21:20 +0200

    Add min_coverage to setup.py

--- setup.py
+++ setup.py
@@ -4,50 +4,52 @@ from setuptools import setup
 
 # Will never support Py2
 if version_info[0] == 2:
-	exit("Sorry, Python 2 is not supported")
+    exit("Sorry, Python 2 is not supported")
 
 # Get the long description from the README file
 here = path.abspath(path.dirname(__file__))
 with open(path.join(here, "README.md"), encoding='utf-8') as f:
-	long_description = f.read()
+    long_description = f.read()
 
 setup(
-	name = "SmallGenomeUtilities",
-	version = "0.0.1",
-	author = "David Seifert",
-	author_email = "david.seifert@bsse.ethz.ch",
-	description = ("A collection of scripts that are useful for dealing with viral RNA NGS data."),
-	license = "GPL2+",
-	keywords = "NGS SAM BAM HIV-1 alignment",
-	url = "https://github.com/SoapZA/SmallGenomeUtilities",
-	packages=["SmallGenomeUtilities"],
-	scripts=[
-		"scripts/convert_qr",
-		"scripts/convert_reference",
-		"scripts/coverage_stats",
-		"scripts/extract_sam",
-		"scripts/extract_seq",
-		"scripts/mapper",
-                "scripts/minority_freq",
-		"scripts/pair_sequences",
-                "scripts/predict_num_reads",
-		"scripts/remove_gaps_msa",
-	],
-	install_requires=[
-		"biopython",
-		"progress",
-		"pysam",
-                "numpy",
-	],
-	long_description=long_description,
-	classifiers=[
-		"Development Status :: 3 - Alpha",
-		"Topic :: Scientific/Engineering :: Bio-Informatics",
-		"Topic :: Utilities",
-		"License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
-		"Programming Language :: Python :: 3",
-		"Programming Language :: Python :: 3.4",
-		"Programming Language :: Python :: 3.5",
-		"Programming Language :: Python :: 3.6",
-	]
+    name="SmallGenomeUtilities",
+    version="0.0.1",
+    author="David Seifert",
+    author_email="david.seifert@bsse.ethz.ch",
+    description=(
+        "A collection of scripts that are useful for dealing with viral RNA NGS data."),
+    license="GPL2+",
+    keywords="NGS SAM BAM HIV-1 alignment",
+    url="https://github.com/SoapZA/SmallGenomeUtilities",
+    packages=["SmallGenomeUtilities"],
+    scripts=[
+        "scripts/convert_qr",
+        "scripts/convert_reference",
+        "scripts/coverage_stats",
+        "scripts/extract_sam",
+        "scripts/extract_seq",
+        "scripts/mapper",
+        "scripts/min_coverage",
+        "scripts/minority_freq",
+        "scripts/pair_sequences",
+        "scripts/predict_num_reads",
+        "scripts/remove_gaps_msa",
+    ],
+    install_requires=[
+        "biopython",
+        "progress",
+        "pysam",
+        "numpy",
+    ],
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ]
 )
