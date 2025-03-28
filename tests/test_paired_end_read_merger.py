2fdffe0922b21a3e4ad616b481c03f70991207e1
Gordon J. Köhn <gordon.koehn@dbsse.ethz.ch>
2025-03-28 11:36:51 +0100

    add unpaired

--- tests/test_paired_end_read_merger.py
+++ tests/test_paired_end_read_merger.py
@@ -1,7 +1,5 @@
 import subprocess
 from pathlib import PurePath
-import json
-import pytest
 
 
 def test_paired_end_read_merger(tmp_path):
@@ -22,3 +20,42 @@ def test_paired_end_read_merger(tmp_path):
     # check output
     with open(exp, "rt") as expf, open(out, "rt") as outf:
         assert [r for r in expf] == [row for row in outf]
+
+
+def test_paired_end_read_merger_with_unpaired_unaligned(tmp_path):
+    # Test with unaligned and unpaired outputs
+    datapath = PurePath("tests/test_paired_end_read_merger")
+    
+    # Output files
+    out = tmp_path / "test.sam"
+    unpaired = tmp_path / "unpaired.sam"
+    unaligned = tmp_path / "unaligned.sam"
+    
+    # Expected files
+    exp_merged = datapath / "mini_merged_with_unpaired.sam"
+    exp_unpaired = datapath / "mini_unpaired.sam"
+    exp_unaligned = datapath / "mini_unaligned.sam"
+    
+    subprocess.check_call(
+        [
+            "paired_end_read_merger",
+            f"--output={out}",
+            f"--unpaired={unpaired}",
+            f"--unaligned={unaligned}",
+            datapath / "mini.sam",
+        ]
+    )
+    
+    # Check main output
+    with open(exp_merged, "rt") as expf, open(out, "rt") as outf:
+        assert [r for r in expf] == [row for row in outf]
+    
+    # Check unpaired output
+    with open(exp_unpaired, "rt") as expf, open(unpaired, "rt") as outf:
+        assert [r for r in expf] == [row for row in outf]
+    
+    # Check unaligned output 
+    with open(exp_unaligned, "rt") as expf, open(unaligned, "rt") as outf:
+        assert [r for r in expf] == [row for row in outf]
+
+
