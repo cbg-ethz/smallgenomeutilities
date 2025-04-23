37187c1e57a090ae77d833f209f33b2c0ddf129b
Ivan Blagoev Topolsky <ivan.topolsky@bsse.ethz.ch>
2025-04-23 23:31:30 +0200

    unit tests for the additional formats
    - test support for IRMA's and samtool depth's tables

--- tests/test_coverage_depth_qc.py
+++ tests/test_coverage_depth_qc.py
@@ -1,5 +1,5 @@
 import subprocess
-from pathlib import PurePath
+from pathlib import PurePath, Path
 import json
 import pytest
 
@@ -50,6 +50,64 @@ def test_covdpqc_with_namedepthsize(tmp_path):
             "got",
             "--",
             datapath / "coverage.tsv",
+        ]
+    )
+
+    # check output
+    with open(exp, "rt") as expf, open(out, "rt") as outf:
+        assert json.load(expf) == json.load(outf)
+
+
+def test_covdpqc_irma(tmp_path):
+    # test IRMA's coverages tables
+    datapath = PurePath("tests/test_coverage_depth_qc")
+
+    exp = datapath / "qc_irma.json"  # expected
+    out = tmp_path / "qc_irma.json"  # current
+
+    subprocess.check_call(
+        [
+            "coverage_depth_qc",
+            f"--output={out}",
+            datapath / PurePath("results/tables/A_NA_N1-coverage.txt"),
+        ]
+    )
+
+    # check output
+    with open(exp, "rt") as expf, open(out, "rt") as outf:
+        assert json.load(expf) == json.load(outf)
+
+
+def test_covdpqc_samtools(tmp_path):
+    # prepare data by reusing 1 file out of frameshift's test data
+    frameshift_data_path = PurePath("tests/test_frameshift_deletions_checks")
+    tmp_data = tmp_path / "coverage.samtools.tsv"
+    subprocess.run(
+        [
+            "samtools",
+            "depth",
+            "-a",
+            "-H",
+            "-J",
+            "-q",
+            "2",
+            "-o",
+            tmp_data,
+            frameshift_data_path / "stoploss_with_insertions.cram",
+        ]
+    )
+
+    # test samtools's depth tables
+    datapath = PurePath("tests/test_coverage_depth_qc")
+
+    exp = datapath / "qc_samtools.json"  # expected
+    out = tmp_path / "qc_samtools.json"  # current
+
+    subprocess.check_call(
+        [
+            "coverage_depth_qc",
+            f"--output={out}",
+            tmp_data,
         ]
     )
 
